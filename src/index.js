93ae462fefc70854465271716056323ba56e107b
fliblip <wickynjy@gmail.com>
2022-09-12 17:58:57 +0300

    Increment likes. wip

--- src/index.js
+++ src/index.js
@@ -1,25 +1,77 @@
-// const apiSource = 'http://localhost:3000'
-
-
 document.addEventListener('DOMContentLoaded', () => {
 
     //get image data(title/likes/comments) from server
 
     fetch('http://localhost:3000/images/1')
     .then(response => response.json())
-    .then((data) => putElement(data))
+    .then(data => {
+        const cardTitle = document.querySelector('#card-title');
+        const cardImage = document.querySelector('#card-image');
+        let likesCount = document.querySelector('#like-count')
 
-    // .then(data => {
+        cardTitle.textContent = data.title
+        likesCount.innerText = data.likes
 
-    // })
+        function getComments(){
+            fetch('http://localhost:3000/comments')
+            .then((response) => response.json())
+            .then((data) => {
+                data.forEach(comment => renderComment(comment.content))
+            })
+        }
+
+        function renderComment(){
+            //get comments-list
+            // const comments = document.querySelector('#comments-list li')
+            let commentList = document.querySelector('#comments-list li')
+            commentContents.innerText = `
+            <li>${comment}</li>`
+            commentList.appendChild(commentContents)
+            
+        
+        }
+
+        //listen for a click event on like button
+        document.querySelector('.like-button').addEventListener('click', increment)
+
+        function increment(){
+            // num = document.querySelector('.likes')
+            // num.textContent++
+            likesCount.textContent++
+        }
+
+
+        //Post comments
+        // document.querySelector('.comment-button').addEventListener('click', addComment (e))
+
+        // function addComment(e){
+        //     let message = e.target.value
+        //     let p = document.createElement('p').append(message)
+        //     document.querySelector('comment-form').appendChild(p)
+        // }
+
+})
 })
 
 
-function putElement(){
-    //select dom element
-    let card = document.querySelector('#card-title')
-    card.innerText = `${data.title}`
-}
+
+// async function 
+//fetch comments
+//     fetch('http://localhost:3000/images/')
+//     .then(response => response.json())
+//     .then(console.log(response))
+//     // .then(data => data.forEach(comment => renderComment(comment)))
+//     // .then(console.log(data))
+
+
+// })
+
+
+// function renderTitle(){
+//     //select dom element
+//     let card = document.querySelector('#card-title')
+//     card.innerText = `${data.title}`
+// }
 
 
 
